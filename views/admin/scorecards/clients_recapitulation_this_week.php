f123bf7c78aa45493de9dd67c070020077b413b0
4ndr1sw <4ndr1sw@gmail.com>
2022-07-08 12:20:38 +0700

    Update clients_recapitulation_this_week.php

--- views/admin/scorecards/clients_recapitulation_this_week.php
+++ views/admin/scorecards/clients_recapitulation_this_week.php
@@ -48,8 +48,8 @@
                                               <tr>
                                                   <th>No.</th>
                                                   <th><?php echo _l('client'); ?></th>
-                                                  <th><?php echo _l('staff'); ?></th>
                                                   <th><?php echo _l('equipment'); ?></th>
+                                                  <th><?php echo _l('staff'); ?></th>
                                                  
                                                   <th><?php echo 'N'; ?></th>
                                                   <th><?php echo 'R'; ?></th>
@@ -74,8 +74,8 @@
                                                             echo $scorecard->company .'<div class="italic">#'. $scorecard->project_name .'</div>#' . $scorecard->start_date; 
                                                         ?>
                                                       </td>
-                                                      <td><?php echo $scorecard->staff_name; ?> </td>
                                                       <td><?php echo $scorecard->tag_name; ?> </td>
+                                                      <td><?php echo $scorecard->staff_name; ?> </td>
                                                       
                                                       <td><?php echo $scorecard->task_status_1; ?> </td>
                                                       <td><?php echo $scorecard->task_status_4; ?> </td>
